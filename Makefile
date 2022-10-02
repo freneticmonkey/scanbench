82a5ec4b9bfd20af440d03df86dbc91e4dfad0a6
Scott Porter <scottporter@neuroticstudios.com>
2022-10-02 13:41:51 +1030

    (mod) improved go speed using more efficient WalkDir function

--- Makefile
+++ Makefile
@@ -5,14 +5,23 @@ go-all:
 go-run:
 	make -C go run
 
+go-run-prof:
+	make -C go run-prof
+
 c-all:
 	make -C c all
 
 c-run:
 	make -C c run
 
-all: go-all c-all
+v-all:
+	make -C v all
 
-run: c-run go-run
+v-run:
+	make -C v run
+
+all: go-all c-all v-all
+
+run: c-run go-run v-run
 	
 
--- go/scanner/main.go
+++ go/scanner/main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"context"
 	"flag"
+	"io/fs"
 	"log"
 	"os"
 	"path/filepath"
+	"runtime/pprof"
+	"runtime/trace"
 	"sync"
 	"time"
 )
@@ -13,26 +17,32 @@ var wg sync.WaitGroup
 var num_files int
 var num_files_mut sync.Mutex
 
-func walkDir(dir string) {
+func walkDir(ctx context.Context, dir string) {
+
+	wg.Add(1)
 	defer wg.Done()
 
+	ctx, task := trace.NewTask(ctx, "walkDir")
+	trace.Log(ctx, "path", dir)
+	defer task.End()
+
 	file_count := 0
-	visit := func(path string, f os.FileInfo, err error) error {
-		if f.IsDir() && path != dir {
-			wg.Add(1)
-			go walkDir(path)
+
+	visitDir := func(path string, d fs.DirEntry, err error) error {
+		if d.IsDir() && path != dir {
+			go walkDir(ctx, path)
 			return filepath.SkipDir
 		}
 
 		// for now don't do anything with each file
-		if f.Mode().IsRegular() {
-			// fmt.Printf("Visited: %s File name: %s Size: %d bytes\n", path, f.Name(), f.Size())
+		if d.Type().IsRegular() {
 			file_count++
 		}
 		return nil
 	}
 
-	filepath.Walk(dir, visit)
+	filepath.WalkDir(dir, visitDir)
+
 	num_files_mut.Lock()
 	num_files += file_count
 	num_files_mut.Unlock()
@@ -41,12 +51,38 @@ func walkDir(dir string) {
 func main() {
 
 	var (
-		root string
-		err  error
+		root      string
+		profile   string
+		traceFile string
+		err       error
 	)
 
 	flag.StringVar(&root, "path", "", "path of directory to scan.  Uses current working directory if not provided")
+	flag.StringVar(&profile, "cpu-profile", "", "write cpu profile to file")
+	flag.StringVar(&traceFile, "trace", "", "write trace to file")
 	flag.Parse()
+
+	if profile != "" {
+		f, err := os.Create(profile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer f.Close()
+
+		pprof.StartCPUProfile(f)
+		defer pprof.StopCPUProfile()
+	}
+
+	if traceFile != "" {
+		f, err := os.Create(traceFile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer f.Close()
+
+		trace.Start(f)
+		defer trace.Stop()
+	}
 
 	if root == "" {
 		root, err = os.Getwd()
@@ -58,6 +94,8 @@ func main() {
 
 	log.Printf("Scanning path: %s", root)
 
+	pctx := context.Background()
+
 	_, err = os.Stat(root)
 
 	if err != nil {
@@ -66,12 +104,13 @@ func main() {
 
 	start := time.Now()
 
-	wg.Add(1)
-	walkDir(root)
+	walkDir(pctx, root)
+
 	wg.Wait()
 
 	elapsed := time.Since(start)
 
-	log.Printf("Go filewalk took %s", elapsed)
-	log.Printf("Num files read %d", num_files)
+	log.Printf("Go filewalk took: %s", elapsed)
+	log.Printf("Num files read:   %d", num_files)
+	log.Println("Num files expected: 1048576")
 }
