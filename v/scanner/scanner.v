095ac0fc0fb8b240a1cb24afd2691fff5de55045
Scott Porter <scottporter@neuroticstudios.com>
2022-10-03 01:18:13 +1030

    (mod) v - implemented concurrent file scanning

--- v/scanner/scanner.v
+++ v/scanner/scanner.v
@@ -1,27 +1,78 @@
 import os
+import time
 
-fn path_scan(path string) {
-	dirs := []string{}
-	files := os.ls(path) or {
-		println(err)
-		return
-	}
+fn path_scan(path string, mut file_count &int, do_walk bool) {
+	// mut fc := 0
+	// mut fc_ref := &fc
 	
-	for file in files {
-		if os.is_file(file) {
-			println(file)
+	
+	if do_walk {
+		visit := fn [mut file_count] (path string) {
+			// println(path)
+			if os.is_dir(path) {
+				
+			} else {
+				(*file_count)++
+			}
 		}
-		else if os.is_dir(file) {
-			append(dirs, file)
+
+		os.walk(path, visit)
+	} else {
+		mut dirs := []string{}
+		
+		items := os.ls(path) or {
+			return
 		}
+
+		for pathitem in items {
+			// println("pathitem: $pathitem")
+			full := os.join_path(path, pathitem)
+			if os.is_dir(full) {
+				dirs << full
+			} else {
+				file_count++
+			}
+		}
+
+		mut threads := []thread{}
+		for dir in dirs {
+			threads << go path_scan(dir, mut file_count, false)
+		}
+		threads.wait()
 	}
 
-	for dir in dirs {
-		path_scan(dir)
-	}
 }
 
 fn main() {
-	println("Hello from v!")
-	path_scan("../data")
+	mut file_count := 0
+
+	folder := "data"
+	
+	
+	if false {
+		println("Walk Scanning: $folder")
+
+		start := time.now()
+
+		path_scan(folder, mut &file_count, true)
+
+		elapsed := time.since(start)
+
+		println("Walk Scan completed in: $elapsed")
+		println("Found file count: $file_count")
+		println("")
+		file_count = 0
+	}
+
+	println("Concurrent Scanning: $folder")
+
+	c_start := time.now()
+
+	path_scan(folder, mut &file_count, false)
+
+	c_elapsed := time.since(c_start)
+
+	println("Concurrent Scan completed in: $c_elapsed")
+	println("Found file count: $file_count")
+
 }
